f7e1e2dcc25052765d06772b6c8b1ea08d65063d
Shivam Patel <yodashiv@gmail.com>
2020-07-03 19:55:10 -0700

    Added more info to footnote

--- src/components/Footnote.js
+++ src/components/Footnote.js
@@ -38,12 +38,12 @@ const Footnote = () => {
     return (
         <div className={classes.backgroundContainer}>
             <div className={classes.footNoteContainer}>
-                <h5 id="beginMethodology" className={classes.titleText}> Determining Rt</h5>
+                <h5 id="beginMethodology" className={classes.titleText}> How do we determine Rt?</h5>
                 <p className={classes.explainerText}>
                     Rt is a dynamic number that relies on hard-to-measure factors.
                     Thus, at best, we can only estimate the Rt value of a given region
                     through robust statistical techniques. Inspiration for this project
-                    comes from Mike Kreiger and Kevin Systrom’s website, and the model used
+                    comes from Mike Kreiger and Kevin Systrom, and the model used
                     here is heavily adapted from theirs. For a more in-depth exploration
                     of the math, please reference <a
                     href={"http://systrom.com/blog/the-metric-we-need-to-manage-covid-19/"}
@@ -54,9 +54,28 @@ const Footnote = () => {
                 </p>
                 <h5 id="beginFAQ" className={classes.titleText}> Where do we source our data?</h5>
                 <p className={classes.explainerText}>
-                    Data comes from lorem ipsum. Please note that while we make every
-                    effort to source reliable data, lags and errors in reporting can
+                    Our data comes from Our World In Data, a research effort based out of the University of Oxford.
+                    Please note that while we make every effort to source reliable data, lags and errors in reporting can
                     adversely affect testing data.
+                </p>
+                <h5 className={classes.titleText}> How should this data be used?</h5>
+                <p className={classes.explainerText}>
+                    The Rt values of countries are meant to provide a rough sense of how the virus is growing
+                    in a given country. Please keep in mind that while the Rt value yields valuable insight
+                    into the evolution of COVID-19, other metrics should also be considered to form a
+                    more complete picture of the situation.
+                </p>
+                <h5 className={classes.titleText}> What are the yellow bands on the charts?</h5>
+                <p className={classes.explainerText}>
+                    Because the Rt value is statistically determined, we cannot be certain that the Rt value
+                    revealed by our model reflects reality. Thus, to account for the uncertainty in our model,
+                    we include a margin of error in our calculations. This is visually represented as yellow bands.
+                </p>
+                <h5 className={classes.titleText}> My country is faring well, but its Rt value is high. Why?</h5>
+                <p className={classes.explainerText}>
+                    The Rt value estimates the number of healthy people that contract COVID-19 from an infectious person.
+                    Thus, if there is only 1 person with the virus and they infect 5 other people, the Rt value will be 5.0.
+                    Because of this, it's important to analyze total case count of a country in tandem with its Rt value.
                 </p>
             </div>
         </div>
